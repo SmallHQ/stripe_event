57cf79da395297e574afb2ebc3d7573bac8b7b7c
Daniel Whalen <dwhalen@integrallis.com>
2013-03-06 17:54:21 -0700

    No longer need to maintain a list of event types

--- lib/stripe_event.rb
+++ lib/stripe_event.rb
@@ -1,4 +1,3 @@
-require "set"
 require "stripe"
 require "stripe_event/engine"
 
@@ -38,41 +37,4 @@ module StripeEvent
   self.backend = ActiveSupport::Notifications
   self.event_retriever = lambda { |params| Stripe::Event.retrieve(params[:id]) }
   self.prefix = 'stripe_event'
-
-  TYPE_LIST = Set[
-    'account.updated',
-    'account.application.deauthorized',
-    'charge.succeeded',
-    'charge.failed',
-    'charge.refunded',
-    'charge.dispute.created',
-    'charge.dispute.updated',
-    'charge.dispute.closed',
-    'customer.created',
-    'customer.updated',
-    'customer.deleted',
-    'customer.subscription.created',
-    'customer.subscription.updated',
-    'customer.subscription.deleted',
-    'customer.subscription.trial_will_end',
-    'customer.discount.created',
-    'customer.discount.updated',
-    'customer.discount.deleted',
-    'invoice.created',
-    'invoice.updated',
-    'invoice.payment_succeeded',
-    'invoice.payment_failed',
-    'invoiceitem.created',
-    'invoiceitem.updated',
-    'invoiceitem.deleted',
-    'plan.created',
-    'plan.updated',
-    'plan.deleted',
-    'coupon.created',
-    'coupon.deleted',
-    'transfer.created',
-    'transfer.updated',
-    'transfer.failed',
-    'ping'
-  ].freeze
 end
